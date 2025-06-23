7da4dd1cced22cd84464342b832b4f4fe4fcaf40
Wilson Capacho <wiston.98@hotmail.com>
2025-06-22 20:56:28 -0500

    problema solucionado en el crud

--- resources/js/components/bexsolutions/Crud.vue
+++ resources/js/components/bexsolutions/Crud.vue
@@ -7,6 +7,8 @@ const props = defineProps({
 
 const editing = ref(false)
 const feedback = ref('')
+const feedbackType = ref('') // 'success' o 'error'
+const loading = ref(false)
 const visits = ref([...props.visits])
 
 const notify = (message, type = 'success') => {
@@ -18,9 +20,9 @@ const notify = (message, type = 'success') => {
 
 const submit = async () => {
   const url = editing.value ? `/api/v1/visits/${form.id}` : '/api/v1/visits'
-  const method = editing.value ? 'put' : 'post'
+  const method = editing.value ? 'patch' : 'post'
 
-  
+  loading.value = true
   try {
     const res = await axios[method](url, { ...form })
 
@@ -40,8 +42,11 @@ const submit = async () => {
     }
   } catch {
     notify('Error al guardar', 'error')
+  } finally {
+    loading.value = false
   }
 }
+
 
 const editVisit = (visit) => {
   console.log('visit', visit);
@@ -51,12 +56,15 @@ const editVisit = (visit) => {
 }
 
 const deleteVisit = async (id) => {
+  loading.value = true
   try {
     await axios.delete(`/api/v1/visits/${id}`)
     visits.value = visits.value.filter(v => v.id !== id)
     notify('Visita eliminada', 'success')
   } catch {
     notify('Error al eliminar', 'error')
+  } finally {
+    loading.value = false
   }
 }
 
@@ -81,17 +89,25 @@ const resetForm = () => {
 
 
 <template>
-    <div v-if="feedback" :class="feedbackType === 'success' ? 'text-green-600' : 'text-red-600'" class="mb-2">
+    <div 
+        v-if="feedback" 
+        :class="feedbackType === 'success' ? 'bg-green-600' : 'bg-red-600'" 
+        class="mb-4 text-white px-4 py-3 rounded shadow transition duration-300 ease-in-out"
+      >
          {{ feedback }}
+    </div>
+
+    <div v-if="loading" class="flex justify-center my-2">
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><rect width="7.33" height="7.33" x="1" y="1" fill="#f23900"><animate id="svgSpinnersBlocksWave0" attributeName="x" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="1;4;1"/><animate attributeName="y" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="1;4;1"/><animate attributeName="width" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="8.33" y="1" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="8.33;11.33;8.33"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="1;4;1"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="1" y="8.33" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="1;4;1"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="8.33;11.33;8.33"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="15.66" y="1" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="15.66;18.66;15.66"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="1;4;1"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="8.33" y="8.33" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="8.33;11.33;8.33"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="8.33;11.33;8.33"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="1" y="15.66" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="1;4;1"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="15.66;18.66;15.66"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="15.66" y="8.33" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="15.66;18.66;15.66"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="8.33;11.33;8.33"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="8.33" y="15.66" fill="#f23900"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="8.33;11.33;8.33"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="15.66;18.66;15.66"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33"/></rect><rect width="7.33" height="7.33" x="15.66" y="15.66" fill="#f23900"><animate id="svgSpinnersBlocksWave1" attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="15.66;18.66;15.66"/><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="15.66;18.66;15.66"/><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="7.33;1.33;7.33"/><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="7.33;1.33;7.33"/></rect></svg>
     </div>
     <div class="font-sans">
                     <div class="flex justify-center  ">
                         <div class="w-full">
                             <div class="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md mb-3">
-                                <form method="#" action="#" class=" w-full">
+                                <div method="#" action="#" class=" w-full">
                                                     
-                                    <div class="flex justify-around items-center">
-                                        <div class="mt-7">
+                                    <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+                                        <div class="mt-7 col-span-1">
                                             <label for="" class="text-gray-700">Cliente</label>
                                             <input 
                                                 type="text" 
@@ -101,7 +117,7 @@ const resetForm = () => {
                                                 >
                                         </div>
                             
-                                        <div class="mt-7">
+                                        <div class="mt-7 col-span-1">
                                             <label for="" class="text-gray-700">Email</label>
                                             <input 
                                                 type="email" 
@@ -111,7 +127,7 @@ const resetForm = () => {
                                                 >
                                         </div>
 
-                                        <div class="mt-7">
+                                        <div class="mt-7 col-span-1">
                                             <label for="" class="text-gray-700">Latitud</label>
                                             <input 
                                                 type="text" 
@@ -121,7 +137,7 @@ const resetForm = () => {
                                                 >
                                         </div>
 
-                                        <div class="mt-7">
+                                        <div class="mt-7 col-span-1">
                                             <label for="" class="text-gray-700">Longitud</label>
                                             <input 
                                                 type="text" 
@@ -133,11 +149,14 @@ const resetForm = () => {
                                     </div>
 
                                     <div class="mt-7 flex justify-center">
-                                        <button class="bg-orange-500 w-60 py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                                        <button 
+                                            id="transacction"
+                                            @click.prevent="submit"
+                                            class="bg-orange-500 w-60 py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
                                             {{ editing ? 'Actualizar' : 'Registrar' }}
                                         </button>
                                     </div>
-                                </form>
+                                  </div>
                             </div>
 
                             <div class="relative w-full rounded-3xl  px-6 py-4 bg-gray-100  shadow-md mb-3">
@@ -159,14 +178,6 @@ const resetForm = () => {
                                                     </tr>
                                                 </thead>
                                                 <tbody class="bg-gray-100 rounded-b-3xl">
-                                                    
-                                                    <!-- <tr>
-                                                        <td class="text-center">wilson</td>
-                                                        <td class="text-center">wilson</td>
-                                                        <td class="text-center">wilson</td>
-                                                        <td class="text-center">wilson</td>
-                                                        <td class="text-center">wilson</td>
-                                                    </tr> -->
                                                     <tr v-for="v in visits" :key="v.id" class="border-b-gray-950 hover:bg-orange-100 bg-gray-100">
                                                         <td class="p-1 text-center">{{v.name}}</td>
                                                         <td class="p-1 text-center">{{ v.email }}</td>
